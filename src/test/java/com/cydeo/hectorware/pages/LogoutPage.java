d78c0d6f8fd6e555433eeb8232cad790aba09a7c
M.Sait TURE <turesait@gmail.com>
2023-03-23 16:51:25 +0100

    I created LogoutPage.java

---
+++ src/test/java/com/cydeo/hectorware/pages/LogoutPage.java
@@ -0,0 +1,22 @@
+package com.cydeo.hectorware.pages;
+
+import com.cydeo.hectorware.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LogoutPage {
+    public LogoutPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+    @FindBy(id="settings")
+    public WebElement profileSettingButton;
+    @FindBy(id="expanddiv")
+    public WebElement settingsMenu;
+
+    @FindBy(id="logout")
+    public WebElement logOutButton;
+
+}
